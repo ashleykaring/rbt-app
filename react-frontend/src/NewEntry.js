780f1a63ad1a13acce9857b29090437eb6286e9c
ashleykaring <aaring@calpoly.edu>
2024-10-22 13:28:35 -0700

    edit base frontend files and delete react logo file

---
+++ react-frontend/src/NewEntry.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+
+function NewEntry(props) {
+    const [entry, setEntry] = useState(
+        {
+            rose: "",
+            bud: "",
+            thorn:""
+        }
+    );
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setEntry(prevEntry => ({
+            // keep previous entry content same
+            ...prevEntry,
+            [name]: value
+        }));
+    }
+
+    // function handleChange(event) {
+    //     const { name, value } = event.target;
+        
+    //     if (name === "rose") {
+    //         setEntry(prevEntry => ({
+    //             ...prevEntry,
+    //             rose: value
+    //         }));
+    //     } else if (name === "bud") {
+    //         setEntry(prevEntry => ({
+    //             ...prevEntry,
+    //             bud: value
+    //         }));
+    //     } else if (name === "thorn") {
+    //         setEntry(prevEntry => ({
+    //             ...prevEntry,
+    //             thorn: value
+    //         }));
+    //     }
+    // }
+
+    function submitEntry() {
+        props.handleSubmit(entry);
+        setEntry({rose: "", bud: "", thorn: ""});
+     }
+
+    return(
+        <form>
+            <label htmlFor="rose">Rose</label>
+            <input
+                type="text"
+                name="rose"
+                id="rose"
+                value={entry.rose}
+                onChange={handleChange} 
+            />
+            <label htmlFor="bud">Bud</label>
+            <input
+                type="text"
+                name="bud"
+                id="bud"
+                value={entry.bud}
+                onChange={handleChange} 
+            />
+            <label htmlFor="thorn">Thorn</label>
+            <input
+                type="text"
+                name="thorn"
+                id="thorn"
+                value={entry.thorn}
+                onChange={handleChange} 
+            />
+            <input type="button" value="Submit" onClick={submitEntry} />  
+        </form>
+    );
+}
+
+export default NewEntry;
