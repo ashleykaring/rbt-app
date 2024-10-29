4f3e25cb92a7a063b199fef9841963b9bd2a5541
Brady Welsh <blwelsh@calpoly.edu>
2024-10-29 13:58:20 -0700

    user-services.js now exports various functions which connected and modify the database

--- express-backend/models/user-services.js
+++ express-backend/models/user-services.js
@@ -1,18 +1,103 @@
-const mongoose = require('mongoose');
-const Schema = require("./user")
+import mongoose from "mongoose";
+import {userSchema, entrySchema, userEntriesSchema} from "./user.js"
 
-const userSchema = Schema.userSchema;
-const entrySchema = Schema.entrySchema;
-const userEntriesSchema = Schema.userEntriesSchema;
+const uSchema = userSchema;
+const eSchema = entrySchema;
+const ueSchema = userEntriesSchema;
+
+
+/*
+mongoose.connect("mongodb+srv://bwelsh0707:o02SFQamJRzEiW6T@cluster0.q1r5h.mongodb.net/").then((ans) => {
+    console.log("ConnectedSuccessful");
+  }).catch((err) => {
+    console.log("Error");
+});
+*/
+
 
 let dbConnection;
 
 function getDbConnection() {
     if (!dbConnection) {
-        dbConnection = mongoose.createConnection("", {
+        dbConnection = mongoose.createConnection("mongodb+srv://bwelsh0707:o02SFQamJRzEiW6T@cluster0.q1r5h.mongodb.net/rbt_users_data", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }); 
     }
     return dbConnection;
 }
+
+async function addUser(user) {
+    const userModel = getDbConnection().model("users", uSchema);
+    try {
+        const userToAdd = new userModel(user);
+        userToAdd.entries = await addUserEntries(userToAdd._id);
+        const savedUser = await userToAdd.save();
+        return savedUser;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function addEntry(entry) {
+    const entryModel = getDbConnection().model("rbt_entries", eSchema);
+    try {
+        const entryToAdd = new entryModel(entry);
+        const savedEntry = await entryToAdd.save();
+        addEntryToEntries(savedEntry._id, entry.user_id);
+        return savedEntry;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    
+}
+
+async function addEntryToEntries(id, userid) {
+    const userEntriesModel = getDbConnection().model("user_entries", ueSchema);
+
+    const finalEntries = await userEntriesModel.findOneAndUpdate({user_id: userid}, {
+        $push: {entries: id}
+    });
+
+    return finalEntries;
+
+}
+
+async function addUserEntries(id) {
+    const userEntriesModel = getDbConnection().model("user_entries", ueSchema);
+
+    const defaultEntries = {
+        user_id: id,
+    };
+    try {
+        const userEntriesToAdd = new userEntriesModel(defaultEntries);
+        const savedEntries = await userEntriesToAdd.save();
+        return savedEntries._id;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+    
+}
+
+async function findUserByUsername(username) {
+    const userModel = getDbConnection().model("users", uSchema);
+    return await userModel.find({'username':username});
+}
+
+async function findUserById(id) {
+    const userModel = getDbConnection().model("users", uSchema);
+    return await userModel.find({'_id':id});
+}
+
+async function getAllEntries(userid) {    
+    const entryModel = getDbConnection().model("rbt_entries", eSchema);
+    return await entryModel.find({'user_id':userid});
+}
+
+
+
+export {addUser, findUserByUsername, addEntry, getAllEntries, findUserById};
