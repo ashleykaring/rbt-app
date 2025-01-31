3e1a5be3c01b540efec0b0a451e9bd2a1addc66b
ashleykaring <aaring@calpoly.edu>
2025-01-30 17:18:05 -0800

    frontend fixes/edits to editing tags, start unifying css variables

--- react-frontend/src/search/SearchPage.js
+++ react-frontend/src/search/SearchPage.js
@@ -5,8 +5,6 @@ import React, { useState, useEffect } from "react";
 import { getCurrentUserId } from "../utils/auth";
 import { useNavigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { CiFolderOn } from "react-icons/ci";
-import { MdFolderOpen } from "react-icons/md";
 import { IoFolderOutline } from "react-icons/io5";
 
 import {
